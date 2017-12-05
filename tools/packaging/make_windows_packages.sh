f44dfb350f55b7050859a3264c75224ffd5002f7
Michał Szprejda <mszprejda@antmicro.com>
2017-12-05 15:40:15 +0100

    [#9323] Add windows packaging script.

---
+++ tools/packaging/make_windows_packages.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+set -u
+
+#change dir to script location
+cd "${0%/*}"
+. common_make_packages.sh
+
+if ! is_dep_available zip
+then
+    exit
+fi
+
+if [ -z "${MINGW_PATH:-}" ]
+then
+    MINGW_PATH="C:/MinGW/bin"
+    echo "MINGW_PATH is not set, using default path ($MINGW_PATH)"
+fi
+if [ -z "${GTKSHARP_PATH:-}" ]
+then
+    GTKSHARP_PATH="C:/Program Files (x86)/GtkSharp/2.12/bin"
+    echo "GTKSHARP_PATH path is not set, using default path ($GTKSHARP_PATH)"
+fi
+
+DIR=renode_$VERSION
+
+. common_copy_files.sh
+
+PACKAGES=output/renode_packages/$TARGET
+OUTPUT=$BASE/$PACKAGES
+
+### copy windows dependencies
+cp "$MINGW_PATH"/libgcc_s_dw2-1.dll $DIR/bin
+cp "$GTKSHARP_PATH"/*.dll $DIR/bin
+cp windows/mingw-license $DIR/licenses
+
+### create windows package
+if $REMOVE_WORKDIR
+then
+    zip -qrm $DIR.zip $DIR/
+else
+    zip -qr $DIR.zip $DIR/
+fi
