bf351376feed4008526bfedc92198421a1ac6cfb
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2017-12-06 16:48:37 +0100

    [DEV] Generate assembly version info dynamically in bash.

--- tools/building/createAssemblyInfo.sh
+++ tools/building/createAssemblyInfo.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Create AssemblyInfo.cs but only when the file does not exists or has different git commit
+# Create AssemblyInfo.cs but only when the file does not exists or has different version information
 FILE_NAME="AssemblyInfo"
-CURRENT_VERSION="`git rev-parse --short=8 HEAD`"
-if ! grep "$CURRENT_VERSION" $FILE_NAME.cs > /dev/null 2>/dev/null
+
+CURRENT_VERSION=`cat ../../../tools/version`
+CURRENT_INFORMATIONAL_VERSION="`git rev-parse --short=8 HEAD`"
+
+PARAMS=()
+if ! grep "$CURRENT_VERSION" $FILE_NAME.cs > /dev/null 2>/dev/null \
+    || ! grep "$CURRENT_INFORMATIONAL_VERSION" $FILE_NAME.cs > /dev/null 2>/dev/null
 then
-    sed -e "s;%VERSION%;$CURRENT_VERSION-`date +%Y%m%d%H%M`;" $FILE_NAME.template > $FILE_NAME.cs
+    sed -e "s;%VERSION%;$CURRENT_VERSION;" -e "s;%INFORMATIONAL_VERSION%;$CURRENT_INFORMATIONAL_VERSION-`date +%Y%m%d%H%M`;" $FILE_NAME.template > $FILE_NAME.cs
 fi
 
