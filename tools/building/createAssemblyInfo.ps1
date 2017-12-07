38ba9afbbcc233ae32875f2be1fb4c8f3901a35b
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2017-12-07 11:15:39 +0100

    [DEV] Create assembly version info on Windows.

--- tools/building/createAssemblyInfo.ps1
+++ tools/building/createAssemblyInfo.ps1
@@ -1,5 +1,17 @@
 #!/cygdrive/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe
 
-$CURRENT_VERSION = git rev-parse --short=8 HEAD
+$CURRENT_VERSION = Get-Content "..\..\..\tools\version"
+$CURRENT_INFORMATIONAL_VERSION = git rev-parse --short=8 HEAD
 $FILE_NAME = "AssemblyInfo"
-if (!(Test-Path "$FILE_NAME.cs") -or !(Select-String -q $CURRENT_VERSION "$FILE_NAME.cs")) { (-join ((Get-Content "$FILE_NAME.template") -join [Environment]::NewLine)).replace('%VERSION%', ('{0}-{1}' -f $CURRENT_VERSION, (Get-Date -Format yyyyMMddHHmm))) | Set-Content -Path "$FILE_NAME.cs" }
+if ( !(Test-Path "$FILE_NAME.cs") -or
+     !(Select-String -q $CURRENT_VERSION "$FILE_NAME.cs") -or
+     !(Select-String -q $CURRENT_INFORMATIONAL_VERSION "$FILE_NAME.cs"))
+{
+    (-join ((Get-Content "$FILE_NAME.template") -join [Environment]::NewLine)).`
+    replace('%INFORMATIONAL_VERSION%',
+        ('{0}-{1}' -f $CURRENT_INFORMATIONAL_VERSION, (Get-Date -Format yyyyMMddHHmm))).`
+         replace("%VERSION%", $CURRENT_VERSION)|
+        Set-Content -Path "$FILE_NAME.cs"
+}
+
+# keep the new lines
