e08c144c4112dc63b3d21aa8d70d6ae45bbb5045
Mateusz Hołenko <mholenko@antmicro.com>
2017-12-07 14:14:25 +0100

    [FIX] Fix test.sh so it can be run without tty.

--- test.sh
+++ test.sh
@@ -9,6 +9,9 @@ TESTS_RESULTS="$ROOT_PATH/output/tests"
 
 . "${ROOT_PATH}/tools/common.sh"
 
-STTY_CONFIG=`stty -g`
-python -u "`get_path "$ROOT_PATH/tests/run_tests.py"`" --properties-file "`get_path "$ROOT_PATH/output/properties.csproj"`" -r "`get_path "$TESTS_RESULTS"`" -t "`get_path "$TESTS_FILE"`" "$@"
-stty "$STTY_CONFIG"
+STTY_CONFIG=`stty -g 2>/dev/null || true`
+python -u "`get_path "$ROOT_PATH/tests/run_tests.py"`" --properties-file "`get_path "$ROOT_PATH/output/properties.csproj"`" -r "`get_path "$TESTS_RESULTS"`" -t "`get_path "$TESTS_FILE"`" "$@" || true
+if [ -n "${STTY_CONFIG:-}" ]
+then
+    stty "$STTY_CONFIG"
+fi
