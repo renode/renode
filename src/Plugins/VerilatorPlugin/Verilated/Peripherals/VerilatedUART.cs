aac0053a86b068f42337b40b6b2b314f375a1289
Dawid Wojciechowski <dwojciechowski@internships.antmicro.com>
2019-01-17 16:56:02 +0100

    [#12418] Add VerilatedUART infrastructure.

---
+++ src/Plugins/VerilatorPlugin/Verilated/Peripherals/VerilatedUART.cs
@@ -0,0 +1,70 @@
+﻿//
+// Copyright (c) 2010-2019 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Peripherals.Bus;
+using Antmicro.Renode.Peripherals.UART;
+using Antmicro.Renode.Plugins.VerilatorPlugin.Connection.Protocols;
+
+namespace Antmicro.Renode.Peripherals.Verilated
+{
+    [AllowedTranslations(AllowedTranslation.ByteToDoubleWord)]
+    public class VerilatedUART : BaseDoubleWordVerilatedPeripheral, IUART
+    {
+        public VerilatedUART(Machine machine, string simulationFilePath, long frequency, ulong limitBuffer = LimitBuffer, double timeout = DefaultTimeout) : base (machine, simulationFilePath, frequency, limitBuffer, timeout)
+        {
+        }
+
+        public void WriteChar(byte value)
+        {
+            Send((ActionType)UARTActionNumber.UARTRxd, 0, value);
+        }
+
+        // StopBits, ParityBit and BaudRate are not in sync with the verilated model
+        public Bits StopBits { get { return Bits.One; } }
+        public Parity ParityBit { get { return Parity.None; } }
+        public uint BaudRate { get { return 115200; } }
+
+        public event Action<byte> CharReceived;
+
+        protected override void HandleReceived(ProtocolMessage message)
+        {
+            switch(message.ActionId)
+            {
+                case (ActionType)UARTActionNumber.UARTTxd:
+                    CharReceived?.Invoke((byte)message.Data);
+                    break;
+                default:
+                    base.HandleReceived(message);
+                    break;
+            }
+        }
+
+        protected override void HandleInterrupt(ProtocolMessage interrupt)
+        {
+            switch(interrupt.Address)
+            {
+                case RxdInterrupt:
+                    this.Log(LogLevel.Info, "Rxd interrupt: ", interrupt.Address);
+                    break;
+                default:
+                    base.HandleInterrupt(interrupt);
+                    break;
+            }
+        }
+
+        private const ulong RxdInterrupt = 1;
+    }
+
+    // UARTActionNumber must be in sync with Verilator integration library
+    public enum UARTActionNumber
+    {
+        UARTTxd = 11,
+        UARTRxd = 12
+    }
+}
