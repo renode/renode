23f74f8e73bb3cfb5baed28404ce6f3630fb9e90
Mateusz Hołenko <mholenko@antmicro.com>
2019-03-20 12:25:13 +0100

    [#13347] Extract `TestersProvider` base class.

---
+++ src/Renode/RobotFrameworkEngine/TestersProvider.cs
@@ -0,0 +1,104 @@
+//
+// Copyright (c) 2010-2019 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Peripherals;
+
+namespace Antmicro.Renode.RobotFramework
+{
+    internal abstract class TestersProvider<TTester, TPeripheral> where TPeripheral: class, IEmulationElement
+    {
+        public TestersProvider()
+        {
+            testers = new Dictionary<int, TTester>();
+            peripheralsWithTesters = new HashSet<TPeripheral>();
+            EmulationManager.Instance.EmulationChanged += () =>
+            {
+                lock(testers)
+                {
+                    testers.Clear();
+                }
+            };
+        }
+
+        public int CreateNewTester(Func<TPeripheral, TTester> creator, string peripheralName, string machine = null)
+        {
+            lock(testers)
+            {
+                Machine machineObject;
+                if(machine == null)
+                {
+                    if(!EmulationManager.Instance.CurrentEmulation.Machines.Any())
+                    {
+                        throw new KeywordException("There is no machine in the emulation. Could not create tester for peripheral: {0}", peripheralName);
+                    }
+                    machineObject = EmulationManager.Instance.CurrentEmulation.Machines.Count() == 1
+                        ? EmulationManager.Instance.CurrentEmulation.Machines.First()
+                        : null;
+                    if(machineObject == null)
+                    {
+                        throw new KeywordException("No machine name provided. Don't know which one to choose. Available machines: [{0}]",
+                            string.Join(", ", EmulationManager.Instance.CurrentEmulation.Machines.Select(x => EmulationManager.Instance.CurrentEmulation[x])));
+                    }
+                }
+                else if(!EmulationManager.Instance.CurrentEmulation.TryGetMachineByName(machine, out machineObject))
+                {
+                    throw new KeywordException("Machine with name {0} not found. Available machines: [{1}]", machine,
+                            string.Join(", ", EmulationManager.Instance.CurrentEmulation.Machines.Select(x => EmulationManager.Instance.CurrentEmulation[x])));
+                }
+
+                if(!machineObject.TryGetByName(peripheralName, out IPeripheral typeLessPeripheral))
+                {
+                    throw new KeywordException("Peripheral for machine '{0}' not found or of wrong type: '{1}'. Available peripherals: [{2}]", machine, peripheralName,
+                            string.Join(", ", machineObject.GetAllNames()));
+                }
+                var peripheral = typeLessPeripheral as TPeripheral;
+                if(peripheral == null)
+                {
+                    throw new KeywordException("Peripheral for machine '{0}' not found or of wrong type: '{1}'. Available peripherals: [{2}]", machine, peripheralName,
+                            string.Join(", ", machineObject.GetAllNames()));
+                }
+
+                if(peripheralsWithTesters.Contains(peripheral))
+                {
+                    throw new KeywordException("Tester for peripheral '{0}' in machine '{1}' already exists", peripheralName, machine);
+                }
+
+                var tester = creator(peripheral);
+                peripheralsWithTesters.Add(peripheral);
+                testers.Add(peripheralsWithTesters.Count, tester);
+            }
+            return peripheralsWithTesters.Count;
+        }
+
+        protected TTester GetTesterOrThrowException(int? testerId)
+        {
+            lock(testers)
+            {
+                TTester tester;
+                if(testerId == null)
+                {
+                    if(testers.Count != 1)
+                    {
+                        throw new KeywordException("There is more than one tester available - please specify ID of the desired tester.");
+                    }
+                    tester = testers.Single().Value;
+                }
+                else if(!testers.TryGetValue(testerId.Value, out tester))
+                {
+                    throw new KeywordException("Tester for given ID={0} was not found. Did you forget to create the tester?", testerId);
+                }
+                return tester;
+            }
+        }
+
+        private readonly Dictionary<int, TTester> testers;
+        private readonly HashSet<TPeripheral> peripheralsWithTesters;
+    }
+}
--- src/Renode/RobotFrameworkEngine/UartKeywords.cs
+++ src/Renode/RobotFrameworkEngine/UartKeywords.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2019 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -16,21 +16,8 @@ using Antmicro.Renode.Time;
 
 namespace Antmicro.Renode.RobotFramework
 {
-    internal class UartKeywords : IRobotFrameworkKeywordProvider
+    internal class UartKeywords : TestersProvider<TerminalTester, IUART>, IRobotFrameworkKeywordProvider
     {
-        public UartKeywords()
-        {
-            testers = new Dictionary<int, TerminalTester>();
-            uartsWithTesters = new HashSet<IUART>();
-            EmulationManager.Instance.EmulationChanged += () =>
-            {
-                lock(testers)
-                {
-                    testers.Clear();
-                }
-            };
-        }
-
         public void Dispose()
         {
         }
@@ -38,46 +25,12 @@ namespace Antmicro.Renode.RobotFramework
         [RobotFrameworkKeyword]
         public int CreateTerminalTester(string uart, string prompt = null, int timeout = 30, string machine = null)
         {
-            lock(testers)
+            return CreateNewTester(uartObject => 
             {
-                Machine machineObject;
-                if(machine == null)
-                {
-                    if(!EmulationManager.Instance.CurrentEmulation.Machines.Any())
-                    {
-                        throw new KeywordException("There is no machine in the emulation. Could not create tester for peripheral: {0}", uart);
-                    }
-                    machineObject = EmulationManager.Instance.CurrentEmulation.Machines.Count() == 1
-                        ? EmulationManager.Instance.CurrentEmulation.Machines.First()
-                        : null;
-                    if(machineObject == null)
-                    {
-                        throw new KeywordException("No machine name provided. Don't know which one to choose. Available machines: [{0}]",
-                            string.Join(", ", EmulationManager.Instance.CurrentEmulation.Machines.Select(x => EmulationManager.Instance.CurrentEmulation[x])));
-                    }
-                }
-                else if(!EmulationManager.Instance.CurrentEmulation.TryGetMachineByName(machine, out machineObject))
-                {
-                    throw new KeywordException("Machine with name {0} not found. Available machines: [{1}]", machine,
-                            string.Join(", ", EmulationManager.Instance.CurrentEmulation.Machines.Select(x => EmulationManager.Instance.CurrentEmulation[x])));
-                }
-
-                if(!machineObject.TryGetByName(uart, out IUART uartObject))
-                {
-                    throw new KeywordException("Peripheral for machine '{0}' not found or of wrong type: '{1}'. Available peripherals: [{2}]", machine, uart,
-                            string.Join(", ", machineObject.GetAllNames()));
-                }
-                if(uartsWithTesters.Contains(uartObject))
-                {
-                    throw new KeywordException("Terminal tester for peripheral '{0}' in machine '{1}' already exists", uart, machine);
-                }
-
                 var tester = new TerminalTester(TimeInterval.FromSeconds((uint)timeout), prompt);
                 tester.Terminal.AttachTo(uartObject);
-                uartsWithTesters.Add(uartObject);
-                testers.Add(uartsWithTesters.Count, tester);
-            }
-            return uartsWithTesters.Count;
+                return tester;
+            }, uart, machine);
         }
 
         [RobotFrameworkKeyword]
@@ -170,27 +123,6 @@ namespace Antmicro.Renode.RobotFramework
             GetTesterOrThrowException(testerId).CheckIfUartIsIdle(TimeInterval.FromSeconds(timeInSeconds));
         }
 
-        private TerminalTester GetTesterOrThrowException(int? testerId)
-        {
-            lock(testers)
-            {
-                TerminalTester tester;
-                if(testerId == null)
-                {
-                    if(testers.Count != 1)
-                    {
-                        throw new KeywordException("There are more than one terminal tester available - please specify ID of the desired tester.");
-                    }
-                    tester = testers.Single().Value;
-                }
-                else if(!testers.TryGetValue(testerId.Value, out tester))
-                {
-                    throw new KeywordException("Terminal tester for given ID={0} was not found. Did you forget to call `Create Terminal Tester`?", testerId);
-                }
-                return tester;
-            }
-        }
-
         public struct TerminalTesterResult
         {
             public TerminalTesterResult(string line, double timestamp, string[] groups = null)
@@ -204,8 +136,5 @@ namespace Antmicro.Renode.RobotFramework
             public string[] groups;
             public double timestamp;
         }
-
-        private readonly HashSet<IUART> uartsWithTesters;
-        private readonly Dictionary<int, TerminalTester> testers;
     }
 }
