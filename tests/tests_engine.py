9f2182c3e1de2af1288dd1d81379c765765aa72a
Mateusz Holenko <mholenko@antmicro.com>
2019-12-31 11:32:04 +0100

    [FIX] Fix formatting

--- tests/tests_engine.py
+++ tests/tests_engine.py
@@ -12,22 +12,91 @@ registered_handlers = []
 
 def prepare_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("tests", help="List of test files", nargs='*')
-    parser.add_argument("-f", "--fixture",  dest="fixture", help="Fixture to test", metavar="FIXTURE")
-    parser.add_argument("-n", "--repeat",   dest="repeat_count", nargs="?", type=int, const=0, default=1, help="Repeat tests a number of times (no-flag: 1, no-value: infinite)")
-    parser.add_argument("-d", "--debug",    dest="debug_mode",  action="store_true",  default=False, help="Debug mode")
-    parser.add_argument("-o", "--output",   dest="output_file", action="store",       default=None,  help="Output file, default STDOUT.")
-    parser.add_argument("-b", "--buildbot", dest="buildbot",    action="store_true",  default=False, help="Buildbot mode. Before running tests prepare environment, i.e., create tap0 interface.")
-    parser.add_argument("-t", "--tests",    dest="tests_file",  action="store",       default=None,  help="Path to a file with a list of assemblies with tests to run. This is ignored if any test file is passed as positional argument.")
+
+    parser.add_argument("tests",
+                        help="List of test files",
+                        nargs='*')
+
+    parser.add_argument("-f", "--fixture",
+                        dest="fixture",
+                        help="Fixture to test",
+                        metavar="FIXTURE")
+
+    parser.add_argument("-n", "--repeat",
+                        dest="repeat_count",
+                        nargs="?",
+                        type=int,
+                        const=0,
+                        default=1,
+                        help="Repeat tests a number of times (no-flag: 1, no-value: infinite)")
+
+    parser.add_argument("-d", "--debug",
+                        dest="debug_mode",
+                        action="store_true",
+                        default=False,
+                        help="Debug mode")
+
+    parser.add_argument("-o", "--output",
+                        dest="output_file",
+                        action="store",
+                        default=None,
+                        help="Output file, default STDOUT.")
+
+    parser.add_argument("-b", "--buildbot",
+                        dest="buildbot",
+                        action="store_true",
+                        default=False,
+                        help="Buildbot mode. Before running tests prepare environment, i.e., create tap0 interface.")
+
+    parser.add_argument("-t", "--tests",
+                        dest="tests_file",
+                        action="store",
+                        default=None,
+                        help="Path to a file with a list of assemblies with tests to run. This is ignored if any test file is passed as positional argument.")
+
+    parser.add_argument("-T", "--type",
+                        dest="test_type",
+                        action="store",
+                        default="all",
+                        help="Type of test to execute (all by default)")
+
+    parser.add_argument("-r", "--results-dir",
+                        dest="results_directory",
+                        action="store",
+                        default=os.path.join(this_path, 'tests'),
+                        help="Location where test results should be stored.")
+
+    parser.add_argument("--run-gdb",
+                        dest="run_gdb",
+                        action="store_true",
+                        help="Run tests under GDB control.")
+
+    parser.add_argument("--exclude",
+                        default=[],
+                        action="append",
+                        help="Do not run tests marked with a tag.")
+
+    parser.add_argument("--stop-on-error",
+                        dest="stop_on_error",
+                        action="store_true",
+                        default=False,
+                        help="Terminate immediately on the first test failure")
+
     if platform != "win32":
-        parser.add_argument("-p", "--port",     dest="port",        action="store",       default=None,  help="Debug port.")
-        parser.add_argument("-s", "--suspend",  dest="suspend",     action="store_true",  default=False, help="Suspend test waiting for a debugger.")
-    parser.add_argument("-T", "--type",     dest="test_type",   action="store",       default="all", help="Type of test to execute (all by default)")
-    parser.add_argument("-r", "--results-dir",  dest="results_directory",  action="store", default=os.path.join(this_path, 'tests'),  help="Location where test results should be stored.")
-    parser.add_argument("--run-gdb", dest="run_gdb", action="store_true", help="Run tests under GDB control.")
-    parser.add_argument("--exclude", default=[], action="append", help="Do not run tests marked with a tag.")
-    parser.add_argument("--stop-on-error", dest="stop_on_error", action="store_true", default=False, help="Terminate immediately on the first test failure")
+        parser.add_argument("-p", "--port",
+                            dest="port",
+                            action="store",
+                            default=None,
+                            help="Debug port.")
+
+        parser.add_argument("-s", "--suspend",
+                            dest="suspend",
+                            action="store_true",
+                            default=False,
+                            help="Suspend test waiting for a debugger.")
+
     return parser
+
 
 def call_or_die(to_call, error_message):
     ret_code = subprocess.call(to_call)
@@ -35,11 +104,13 @@ def call_or_die(to_call, error_message):
         print(error_message)
         sys.exit(ret_code)
 
+
 def setup_tap():
     call_or_die(['sudo', 'tunctl', '-d', 'tap0'], 'Error while removing old tap0 interface')
     call_or_die(['sudo', 'tunctl', '-t', 'tap0', '-u', str(os.getuid())], 'Error while creating tap0 interface')
     call_or_die(['sudo', '-n', 'ip', 'link', 'set', 'tap0', 'up'], 'Error while setting interface state')
     call_or_die(['sudo', '-n', 'ip', 'addr', 'add', '192.0.2.1/24', 'dev', 'tap0'], 'Error while setting ip address')
+
 
 def handle_options(options):
     if options.buildbot:
@@ -58,8 +129,10 @@ def handle_options(options):
         options.tests = [line.rstrip() for line in open(options.tests_file)]
     options.configuration = 'Debug' if options.debug_mode else 'Release'
 
+
 def register_handler(handler_type, extension, creator, before_parsing=None, after_parsing=None):
     registered_handlers.append({'type': handler_type, 'extension': extension, 'creator': creator, 'before_parsing': before_parsing, 'after_parsing': after_parsing})
+
 
 def run():
     parser = prepare_parser()
