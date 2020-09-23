59d45acf98619223013b697eab84e582d9a4d67e
Adam Jeliński <ajelinski@antmicro.com>
2020-09-23 18:12:06 +0200

    [FIX] conda: Fix activate script
    1. Add parenthesis around paths with variables,
    2. Add `-quit` to stop searching after first `libdl.so.2` found,
    3. Add `|| true` to ignore find's return code; it says very little about
       whether link was created but can make conda fail.

--- tools/packaging/conda/activate.sh
+++ tools/packaging/conda/activate.sh
@@ -2,6 +2,7 @@
 # to avoid renode crashing we add local link on first activation.
 # As we link to the link provided by libc package it should work after update
 
-if [ `uname` = 'Linux' ] && ( ! [ -e $CONDA_PREFIX/lib/libdl.so ] ) && [ `whereis libdl | grep 'libdl\.so' | wc -l` -eq 0 ] ; then
-    find / -name 'libdl.so.2' -exec ln -s {} $CONDA_PREFIX/lib/libdl.so \;
+if [ `uname` = 'Linux' ] && ( ! [ -e "$CONDA_PREFIX/lib/libdl.so" ] ) && [ `whereis libdl | grep 'libdl\.so' | wc -l` -eq 0 ] ; then
+    # Return code is ignored as it doesn't tell whether link was created but may break 'conda install'
+    find / -name 'libdl.so.2' -exec ln -s {} "$CONDA_PREFIX/lib/libdl.so" \; -quit || true
 fi
