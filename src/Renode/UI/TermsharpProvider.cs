d889e694ff78bcf20e3f012542e64d22c2ef8186
Benjamin Cabé <kartben@users.noreply.github.com>
2021-01-11 15:49:13 +0100

    [GH#167] Fix potential NRE in TermsharpProvider

--- src/Renode/UI/TermsharpProvider.cs
+++ src/Renode/UI/TermsharpProvider.cs
@@ -1,5 +1,6 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2021 Antmicro
+// Copyright (c) 2020-2021 Microsoft
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -20,7 +21,7 @@ namespace Antmicro.Renode.UI
             TerminalWidget terminalWidget = null;
             ApplicationExtensions.InvokeInUIThreadAndWait(() =>
             {
-                terminalWidget = new TerminalWidget(() => window.HasFocus, isMonitorWindow);
+                terminalWidget = new TerminalWidget(() => window?.HasFocus ?? false, isMonitorWindow);
             });
             ioSource = terminalWidget.IOSource;
 
