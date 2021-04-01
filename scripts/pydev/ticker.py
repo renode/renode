34ae0def7ec59ed951dcbaec801c72da8fa63683
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-04-01 23:27:23 +0200

    [#25482] pydevs: Expand ticker.py with basic configurability

--- scripts/pydev/ticker.py
+++ scripts/pydev/ticker.py
@@ -1,7 +1,16 @@
+INIT_VALUE = 1
+STEP = 2
+
 if request.isInit:
 	lastVal = 0
+        step = 1
+elif request.isUser:
+        if request.offset == INIT_VALUE:
+            lastVal = request.value
+        elif request.offset == STEP:
+            step = request.value
 else:
-	lastVal = lastVal + 1
+	lastVal = lastVal + step
 	request.value = lastVal
 
 self.NoisyLog("%s on TICKER at 0x%x, value 0x%x" % (str(request.type), request.offset, request.value))
