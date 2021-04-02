8b0334827583ada3197b20533db8bdf52577e5bd
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-04-02 11:34:29 +0200

    [DEV] pydevs: Remove fast-ticker
    It's obsolete now that we have configurability in pydevs

--- scripts/pydev/fast-ticker.py
+++
@@ -1,7 +0,0 @@
-if request.isInit:
-	lastVal = 0
-else:
-	lastVal = lastVal + 100
-	request.value = lastVal
-
-self.NoisyLog("%s on TICKER at 0x%x, value 0x%x" % (str(request.type), request.offset, request.value))
