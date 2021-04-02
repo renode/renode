1a4500e8720ff4094e04590c3f4d51b5ca7e31d0
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-04-02 11:15:09 +0200

    [DEV] ticker.py: Clean up formatting

--- scripts/pydev/ticker.py
+++ scripts/pydev/ticker.py
@@ -2,15 +2,15 @@ INIT_VALUE = 1
 STEP = 2
 
 if request.isInit:
-	lastVal = 0
-        step = 1
+    lastVal = 0
+    step = 1
 elif request.isUser:
-        if request.offset == INIT_VALUE:
-            lastVal = request.value
-        elif request.offset == STEP:
-            step = request.value
+    if request.offset == INIT_VALUE:
+        lastVal = request.value
+    elif request.offset == STEP:
+        step = request.value
 else:
-	lastVal = lastVal + step
-	request.value = lastVal
+    lastVal = lastVal + step
+    request.value = lastVal
 
 self.NoisyLog("%s on TICKER at 0x%x, value 0x%x" % (str(request.type), request.offset, request.value))
