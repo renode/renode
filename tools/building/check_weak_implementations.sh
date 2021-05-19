e4cd2bc791105b9aab6818df031a1dd6e4c2a5ba
Adam Jeliński <ajelinski@antmicro.com>
2021-05-19 13:39:31 +0200

    [FIX] check_weak_implementations: Check renode_memory.c too
    Author: Piotr Zierhoffer <pzierhoffer@antmicro.com>

--- tools/building/check_weak_implementations.sh
+++ tools/building/check_weak_implementations.sh
@@ -17,11 +17,19 @@ pushd ../../src/Infrastructure/src/Emulator/Cores/ > /dev/null
 
 WEAKS=weaks.tmp
 IMPLEMENTATIONS=implementations.tmp
+CALLBACKS_BIN=callbacks.tmp
+PATHS_TO_BIN="renode/renode_callbacks.c renode/renode_memory.c"
+
 ARCHS="arm ppc sparc"
 PATHS_WEAK="tlib/callbacks.c tlib/arch/arm/arch_callbacks.c tlib/arch/ppc/arch_callbacks.c tlib/arch/sparc/arch_callbacks.c"
-PATHS_IMPLEMENTATION="renode/renode_callbacks.c renode/arch/arm/renode_arm_callbacks.c renode/arch/ppc/renode_ppc_callbacks.c renode/arch/sparc/renode_sparc_callbacks.c"
+PATHS_IMPLEMENTATION="$CALLBACKS_BIN renode/arch/arm/renode_arm_callbacks.c renode/arch/ppc/renode_ppc_callbacks.c renode/arch/sparc/renode_sparc_callbacks.c"
 
 RESULT=0
+
+for PATH_TO_BIN in $PATHS_TO_BIN
+do
+    cat $PATH_TO_BIN >> $CALLBACKS_BIN
+done
 
 for PATH_WEAK in $PATHS_WEAK
 do
@@ -39,6 +47,7 @@ done
 
 rm $WEAKS
 rm $IMPLEMENTATIONS
+rm $CALLBACKS_BIN
 
 popd > /dev/null
 
