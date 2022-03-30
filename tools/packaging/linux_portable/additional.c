12b9529b31aaad78cef77b6c3b1163209cc90986
Peter Gielda <pgielda@antmicro.com>
2022-03-30 18:35:44 +0200

    [#33143] portable: Cleanup additional.c

--- tools/packaging/linux_portable/additional.c
+++ tools/packaging/linux_portable/additional.c
@@ -1,23 +1,27 @@
+// 
+// part of renode portable
+//
+
 //
 // reimplement some libc stuff not present in older versions
 // but possibly used by libmono
 //
 
+#include <syscall.h>
+ssize_t __wrap_getrandom (void *buffer, size_t length, unsigned int flags) {
+    return syscall(__NR_getrandom, buffer, length, flags);
+}
+
 void __wrap_powf() {
-	printf("!!!! powf!\n"); // TODO
+    printf("!!!! powf!\n"); // TODO
 }
 
 void __wrap_logf() {
-	printf("!!!! logf!\n"); // TODO
+    printf("!!!! logf!\n"); // TODO
 }
 
 void __wrap_expf() {
-	printf("!!!! expf!\n"); // TODO
-}
-
-#include <syscall.h>
-ssize_t __wrap_getrandom (void *buffer, size_t length, unsigned int flags) {
- return syscall(__NR_getrandom, buffer, length, flags);
+    printf("!!!! expf!\n"); // TODO
 }
 
 extern void* __libc_dlopen_mode(const char* filename, int flag);
@@ -33,12 +37,12 @@ char* list_names[LIST_LENGTH];
 int list_counter = 0;
 
 static char* get_name(void* ptr) {
-       for (int i = 0; i < list_counter; i++) {
-            if (list_ptrs[i] == ptr) {
-                return list_names[i];
-            }
-       }
-       return "(unknown)";
+    for (int i = 0; i < list_counter; i++) {
+        if (list_ptrs[i] == ptr) {
+            return list_names[i];
+        }
+    }
+    return "(unknown)";
 }
 
 #include <dlfcn.h>
@@ -136,28 +140,28 @@ extern void CloseZStream();
 extern void ReadZStream();
 extern void WriteZStream();
 
-// dummy function so that they're used
+// dummy function so that they're not optimized away
 #define ATTR __attribute__ ((__visibility__ ("default"))) __attribute__((noinline))
 ATTR void DO_NOT_RUN_dummy_callback(void) {
-        // the section below contains a list of all
-        // symbols that are to be remapped in the dllmap
-        // by the tools/packaging/make_linux_portable.sh script;
-        // they are extracted automatically, so please
-        // add all new items between markers and
-        // *DO NOT* modify markers themselves
+    // the section below contains a list of all
+    // symbols that are to be remapped in the dllmap
+    // by the tools/packaging/make_linux_portable.sh script;
+    // they are extracted automatically, so please
+    // add all new items between markers and
+    // *DO NOT* modify markers themselves
         
-        // --- REMAPPED SYMBOLS SECTION STARTS ---
-        Mono_Posix_Syscall_get_at_fdcwd();
-        Mono_Posix_Syscall_L_ctermid();
-        Mono_Posix_Syscall_get_utime_now();
-        Mono_Posix_Syscall_readlink();
-        Mono_Posix_Stdlib_SIG_DFL();
-        Mono_Posix_Stdlib_EXIT_FAILURE();
-        CreateZStream();
-        CloseZStream();
-        ReadZStream();
-        WriteZStream();
-        // --- REMAPPED SYMBOLS SECTION ENDS ---
+    // --- REMAPPED SYMBOLS SECTION STARTS ---
+    Mono_Posix_Syscall_get_at_fdcwd();
+    Mono_Posix_Syscall_L_ctermid();
+    Mono_Posix_Syscall_get_utime_now();
+    Mono_Posix_Syscall_readlink();
+    Mono_Posix_Stdlib_SIG_DFL();
+    Mono_Posix_Stdlib_EXIT_FAILURE();
+    CreateZStream();
+    CloseZStream();
+    ReadZStream();
+    WriteZStream();
+    // --- REMAPPED SYMBOLS SECTION ENDS ---
 }
 
 //
@@ -168,34 +172,34 @@ ATTR void DO_NOT_RUN_dummy_callback(void) {
 // helper methods
 //
 ATTR int GetBundlesCount(void) {
-	static int bundle_count = -1;
-	if (bundle_count != -1) return bundle_count;
-        MonoBundledAssembly **ptr = (MonoBundledAssembly **) compressed;
-        int nbundles = 0;
-        while (*ptr++ != NULL) {
-                nbundles++;
-        }
-	bundle_count = nbundles;
-        return bundle_count;
+    static int bundle_count = -1;
+    if (bundle_count != -1) return bundle_count;
+    MonoBundledAssembly **ptr = (MonoBundledAssembly **) compressed;
+    int nbundles = 0;
+    while (*ptr++ != NULL) {
+        nbundles++;
+    }
+    bundle_count = nbundles;
+    return bundle_count;
 }
 
 static MonoBundledAssembly *get_bundle(int id) {
-        return (id >= GetBundlesCount()) ? NULL : bundled[id];
+    return (id >= GetBundlesCount()) ? NULL : bundled[id];
 }
 
 ATTR char* GetBundleName(int id) {
-	MonoBundledAssembly *bundle = get_bundle(id);
-	return bundle ? (char*)(bundle->name) : "";
+    MonoBundledAssembly *bundle = get_bundle(id);
+    return bundle ? (char*)(bundle->name) : "";
 }
 
 ATTR uint32_t GetBundleDataSize(int id) {
-        MonoBundledAssembly *bundle = get_bundle(id);
-        return bundle ? bundle->size : 0;
+    MonoBundledAssembly *bundle = get_bundle(id);
+    return bundle ? bundle->size : 0;
 }
 
 ATTR void* GetBundleDataPointer(int id) {
-	MonoBundledAssembly *bundle = get_bundle(id);
-	return bundle ? (void*)bundle->data : NULL;
+    MonoBundledAssembly *bundle = get_bundle(id);
+    return bundle ? (void*)bundle->data : NULL;
 }
 
 //
@@ -204,44 +208,43 @@ ATTR void* GetBundleDataPointer(int id) {
 
 #include <mono/jit/jit.h>
 int main (int argc, char* argv[]) {
-        char **newargs;
-        int i, k = 0;
+    char **newargs;
+    int i, k = 0;
 
-        // dirname might modify the content, so let's keep
-        // the link in a separate buffer first
-        char tmp[MAX_FILENAME_LENGTH];
-        ssize_t l = readlink("/proc/self/exe", tmp, MAX_FILENAME_LENGTH);
-        // adding 2 to the size to make room for `/` and the final NULL character
-        // in theory 1 should be enough as we call `dirname`, but let's be safe
-        if (l < 0 || l >= MAX_FILENAME_LENGTH - 2)
-        {
-            printf("ERROR: Could read the link to self and determine the binary path. Exiting");
-            return 1;
-        }
-	sprintf(binary_path, "%s/", dirname(tmp));
+    // dirname might modify the content, so let's keep
+    // the link in a separate buffer first
+    char tmp[MAX_FILENAME_LENGTH];
+    ssize_t l = readlink("/proc/self/exe", tmp, MAX_FILENAME_LENGTH);
+    // adding 2 to the size to make room for `/` and the final NULL character
+    // in theory 1 should be enough as we call `dirname`, but let's be safe
+    if (l < 0 || l >= MAX_FILENAME_LENGTH - 2) {
+        printf("ERROR: Could read the link to self and determine the binary path. Exiting");
+        return 1;
+    }
+    sprintf(binary_path, "%s/", dirname(tmp));
 
-        newargs = (char **) malloc (sizeof (char *) * (argc + 2));
-        newargs [k++] = image_name;
+    newargs = (char **) malloc (sizeof (char *) * (argc + 2));
+    newargs [k++] = image_name;
 
-        for (i = 1; i < argc; i++) {
-                newargs [k++] = argv [i];
-        }
-        newargs [k] = NULL;
+    for (i = 1; i < argc; i++) {
+        newargs [k++] = argv [i];
+    }
+    newargs [k] = NULL;
 
-        if (config_dir != NULL && getenv ("MONO_CFG_DIR") == NULL) {
-                mono_set_dirs (getenv ("MONO_PATH"), config_dir);
-        }
+    if (config_dir != NULL && getenv ("MONO_CFG_DIR") == NULL) {
+        mono_set_dirs (getenv ("MONO_PATH"), config_dir);
+    }
 
-        mono_mkbundle_init();
+    mono_mkbundle_init();
 
-        MonoDomain *domain = mono_jit_init ("Renode.exe");
-        MonoAssembly *assembly;
-        assembly = mono_domain_assembly_open (domain, "Renode.exe");
+    MonoDomain *domain = mono_jit_init ("Renode.exe");
+    MonoAssembly *assembly;
+    assembly = mono_domain_assembly_open (domain, "Renode.exe");
 
-        mono_jit_exec (domain, assembly, k, newargs);
-        int retval = mono_environment_exitcode_get ();
-        mono_jit_cleanup (domain);
+    mono_jit_exec (domain, assembly, k, newargs);
+    int retval = mono_environment_exitcode_get ();
+    mono_jit_cleanup (domain);
 
-        return retval;
+    return retval;
 }
 
