78ce8a59ff4aa1fae78f3616a55874f8f3f408ca
Jakub Jatczak <jjatczak@antmicro.com>
2023-01-16 10:55:52 +0100

    [#40237] Add dirty addresses broadcasting

--- src/Renode/PlatformDescription/CreationDriver.cs
+++ src/Renode/PlatformDescription/CreationDriver.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -107,6 +107,7 @@ namespace Antmicro.Renode.PlatformDescription
                 usingsBeingProcessed.Clear();
                 PrepareVariables();
             }
+            machine.PostCreationActions();
         }
 
         private void ValidatePreMerge(string file, string source, string prefix)
