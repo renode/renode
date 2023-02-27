1a0558cc66141b0c5f4c40ec61062470d7de08d8
Michał Szprejda <mszprejda@antmicro.com>
2023-02-27 12:47:42 +0100

    [#42021] HttpServer: Add option to select any available port

--- src/Renode/RobotFrameworkEngine/HttpServer.cs
+++ src/Renode/RobotFrameworkEngine/HttpServer.cs
@@ -1,13 +1,16 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2023 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
 //
 using System;
+using System.IO;
 using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using Antmicro.Renode.Logging;
+using Antmicro.Renode.Utilities;
 
 namespace Antmicro.Renode.RobotFramework
 {
@@ -15,7 +18,6 @@ namespace Antmicro.Renode.RobotFramework
     {
         public HttpServer(XmlRpcServer processor)
         {
-            listener = new HttpListener();
             xmlRpcServer = processor;
             listenerThread = new Thread(Runner)
             {
@@ -24,16 +26,81 @@ namespace Antmicro.Renode.RobotFramework
             };
         }
 
+        // port == 0 is special as it means "select any available port"
         public void Run(int port)
         {
-            Logger.Log(LogLevel.Info, "Robot Framework remote server is listening on port {0}", port);
-#if PLATFORM_WINDOWS
-            listener.Prefixes.Add($"http://localhost:{port}/");
-#else
-            listener.Prefixes.Add($"http://*:{port}/");
-#endif
+            var selectAnyPort = (port == 0);
+            // range 49152-65535 as suggested in https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt for Private Ports
+            var minPort = selectAnyPort ? 49152 : port;
+            var maxPort = selectAnyPort ? 65535 : port;
+
+            if(!TryStartListener(out listener, out var actualPort, minPort, maxPort))
+            {
+                Logger.Log(LogLevel.Error, "Could not start the HTTP server on {0}", selectAnyPort ? "any port" : $"port {port}");
+                return;
+            }
+
+            if(!TryCreatePortFile(actualPort))
+            {
+                return;
+            }
+
+            Logger.Log(LogLevel.Info, "Robot Framework remote server is listening on port {0}", actualPort);
+
             listenerThread.Start();
             listenerThread.Join();
+        }
+
+        private bool TryCreatePortFile(int actualPort)
+        {
+            if(!TemporaryFilesManager.Instance.TryCreateFile(RobotPortFile, out var file))
+            {
+                Logger.Log(LogLevel.Error, "Could not create port file");
+                return false;
+            }
+
+            try
+            {
+                File.WriteAllText(file, actualPort.ToString());
+            }
+            catch(Exception ex)
+            {
+                Logger.Log(LogLevel.Error, "Could not create port file: {0}", ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TryStartListener(out HttpListener listener, out int port, int minPort, int maxPort)
+        {
+            for(port = minPort; port <= maxPort; port++)
+            {
+                listener = new HttpListener();
+#if PLATFORM_WINDOWS
+                listener.Prefixes.Add($"http://localhost:{port}/");
+#else
+                listener.Prefixes.Add($"http://*:{port}/");
+#endif
+                try
+                {
+                    listener.Start();
+                    return true;
+                }
+                catch(SocketException)
+                {
+                    // let's try the next port
+                    continue;
+                }
+                catch(HttpListenerException)
+                {
+                    // let's try the next port under .NET Framework
+                    continue;
+                }
+            }
+
+            listener = null;
+            return false;
         }
 
         public void Shutdown()
@@ -41,7 +108,7 @@ namespace Antmicro.Renode.RobotFramework
             quit = true;
             if(Thread.CurrentThread != listenerThread)
             {
-                listener.Close();
+                listener?.Close();
                 listenerThread.Join();
             }
         }
@@ -55,7 +122,6 @@ namespace Antmicro.Renode.RobotFramework
 
         private void Runner()
         {
-            listener.Start();
             while(!quit)
             {
                 var context = listener.GetContext();
@@ -65,9 +131,11 @@ namespace Antmicro.Renode.RobotFramework
         }
 
         private volatile bool quit;
-        private readonly HttpListener listener;
+        private HttpListener listener;
         private readonly Thread listenerThread;
         private readonly XmlRpcServer xmlRpcServer;
+
+        private const string RobotPortFile = "robot_port";
     }
 }
 
