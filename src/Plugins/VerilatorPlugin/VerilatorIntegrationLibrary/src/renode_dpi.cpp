800d0f7cec0bded8cedea4a555260afb465f1202
Jan Olencki <jolencki@antmicro.com>
2023-06-02 15:14:17 +0200

    [#44438] dpi: Add cpp library files

---
+++ src/Plugins/VerilatorPlugin/VerilatorIntegrationLibrary/src/renode_dpi.cpp
@@ -0,0 +1,61 @@
+//
+// Copyright (c) 2010-2023 Antmicro
+//
+// This file is licensed under the MIT License.
+// Full license text is available in 'licenses/MIT.txt'.
+//
+
+#include "renode_dpi.h"
+#include "communication/socket_channel.h"
+
+static SocketCommunicationChannel *socketChannel;
+
+bool renodeDPIReceive(uint32_t* actionId, uint64_t* address, uint64_t* value)
+{
+    if(!socketChannel->getIsConnected())
+    {
+        return false;
+    }
+    Protocol *message = socketChannel->receive();
+    *actionId = message->actionId;
+    *address = message->addr;
+    *value = message->value;
+    delete message;
+    return true;
+}
+
+void renodeDPIDisconnect()
+{
+    socketChannel->disconnect();
+}
+
+void renodeDPIConnect(int receiverPort, int senderPort, const char* address)
+{
+    socketChannel = new SocketCommunicationChannel();
+    socketChannel->connect(receiverPort, senderPort, address);
+}
+
+bool renodeDPISend(uint32_t actionId, uint64_t address, uint64_t value)
+{
+    if(!socketChannel->getIsConnected())
+    {
+        return false;
+    }
+    socketChannel->sendMain(Protocol(actionId, address, value));
+    return true;
+}
+
+bool renodeDPISendToAsync(uint32_t actionId, uint64_t address, uint64_t value)
+{
+    if(!socketChannel->getIsConnected())
+    {
+        return false;
+    }
+    socketChannel->sendSender(Protocol(actionId, address, value));
+    return true;
+}
+
+void renodeDPILog(int logLevel, const char* data)
+{
+    socketChannel->log(logLevel, data);
+}
