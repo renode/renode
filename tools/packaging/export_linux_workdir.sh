5d0f575468bd752ab2cb693c86327620714830f9
Franciszek Zdobylak <fzdobylak@antmicro.com>
2024-02-06 13:00:00 +0100

    [#53614] tools: packaging: Rename renode-template to renode-mono-template
    This change makes the names of renode-*-template scripts in tools/packaging/linux consistent.
    We have now renode-mono-template and renode-dotnet-template
    
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- tools/packaging/export_linux_workdir.sh
+++ tools/packaging/export_linux_workdir.sh
@@ -36,7 +36,7 @@ echo "MONOVERSION=$MONOVERSION" >> $COMMAND_SCRIPT
 echo "REQUIRED_MAJOR=$MONO_MAJOR" >> $COMMAND_SCRIPT
 echo "REQUIRED_MINOR=$MONO_MINOR" >> $COMMAND_SCRIPT
 # skip the first line (with the hashbang)
-tail -n +2 linux/renode-template >> $COMMAND_SCRIPT
+tail -n +2 linux/renode-mono-template >> $COMMAND_SCRIPT
 $SED_COMMAND "s|/opt/renode|$DIR|" $COMMAND_SCRIPT
 chmod +x $COMMAND_SCRIPT
 
