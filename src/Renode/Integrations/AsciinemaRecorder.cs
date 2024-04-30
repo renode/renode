fda4dd132cba250403c33af5b679f886d4824b3a
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2024-04-30 12:31:06 +0200

    [#57790] AsciinemaRecorder: Handle backslash

--- src/Renode/Integrations/AsciinemaRecorder.cs
+++ src/Renode/Integrations/AsciinemaRecorder.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2022 Antmicro
+// Copyright (c) 2010-2024 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -119,6 +119,7 @@ namespace Antmicro.Renode.Integrations
 
         private static List<Tuple<string, string>> mappings = new List<Tuple<string, string>>
         {
+            {"\\",     "\\\\"},
             {"\u0007", "\\a"},
             {"\u0008", "\\b"},
             {"\u0009", "\\t"},
