a1f6a085a03cb02146edd98dfbceb6749b4223b4
Andrzej Drabarek <adrabarek@antmicro.com>
2024-09-05 17:27:00 +0200

    [#64892] verilated: Allow transaction widths different from 32-bit in AHB DPI integration

--- src/Plugins/VerilatorPlugin/VerilatorIntegrationLibrary/hdl/modules/ahb/renode_ahb_subordinate.sv
+++ src/Plugins/VerilatorPlugin/VerilatorIntegrationLibrary/hdl/modules/ahb/renode_ahb_subordinate.sv
@@ -37,9 +37,10 @@ module renode_ahb_subordinate (
     renode_pkg::valid_bits_e valid_bits;
     renode_pkg::data_t data;
     bit is_error;
+    bit is_invalid;
     transfer_direction_e direction;
 
-    wait_for_transfer(address, valid_bits, direction);
+    wait_for_transfer(address, valid_bits, direction, is_invalid);
 
     // The connection.read call may consume an unknown number of clock cycles.
     // To to make the logic simpler both read and write transactions contain at least one cycle with a deasserted ready.
@@ -47,28 +48,40 @@ module renode_ahb_subordinate (
     bus.hreadyout <= 0;
     @(posedge clk);
 
-    if (direction == Read) begin
-      connection.read(address, valid_bits, data, is_error);
-      bus.hrdata = data_t'(data & valid_bits);
-      if (is_error) connection.log_warning($sformatf("Unable to read data from Renode at address 'h%h", address));
-    end else begin
-      connection.write(address, valid_bits, renode_pkg::data_t'(bus.hwdata) & valid_bits, is_error);
-      if (is_error) connection.log_warning($sformatf("Unable to write data to Renode at address 'h%h", address));
+    if (!is_invalid) begin
+      if (direction == Read) begin
+        connection.read(address, valid_bits, data, is_error);
+        bus.hrdata = data_t'(data & valid_bits);
+        if (is_error) connection.log_warning($sformatf("Unable to read data from Renode at address 'h%h", address));
+      end else begin
+        connection.write(address, valid_bits, renode_pkg::data_t'(bus.hwdata) & valid_bits, is_error);
+        if (is_error) connection.log_warning($sformatf("Unable to write data to Renode at address 'h%h", address));
+      end
     end
-    bus.hresp = Okay;
-    bus.hreadyout <= 1;
+
+    if (is_invalid || is_error) begin
+      bus.hresp = Error;
+      @(posedge clk); // An error response should last two cycles.
+      bus.hreadyout <= 1;
+    end
+    else begin
+      bus.hresp = Okay;
+      bus.hreadyout <= 1;
+    end
   endtask
 
 
-  task static wait_for_transfer(output renode_pkg::address_t address, output renode_pkg::valid_bits_e valid_bits, output transfer_direction_e direction);
+  task static wait_for_transfer(output renode_pkg::address_t address, output renode_pkg::valid_bits_e valid_bits, output transfer_direction_e direction, output bit is_invalid);
+    is_invalid = 0;
     bus.hreadyout <= 1;
     while (!bus.hready || bus.htrans == Idle || bus.htrans == Busy) @(posedge clk);
 
     address = bus.haddr;
     valid_bits = bus.transfer_size_to_valid_bits(bus.hsize);
-    if (valid_bits != renode_pkg::DoubleWord || !bus.are_valid_bits_supported(valid_bits)) begin
-      connection.fatal_error("Incorrect access, the AHB Subordinate currently supports only 32-bit access.");
-    end
     direction = transfer_direction_e'(bus.hwrite);
+    if (!bus.are_valid_bits_supported(valid_bits)) begin
+      connection.log_warning($sformatf("Unsupported transaction width of %d for AHB bus with width %d. No transaction will be performed.", renode_pkg::valid_bits_to_transaction_width(valid_bits), bus.DataWidth));
+      is_invalid = 1;
+    end
   endtask
 endmodule
