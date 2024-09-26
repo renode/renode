97833ca4abebdb95071ecd7e11fa152bb01bad13
Sebastian Kwaśniak <skwasniak@internships.antmicro.com>
2024-09-26 12:05:05 +0200

    [#66352] Asciinema: Add support for CDC ACM UART

--- src/Renode/Integrations/AsciinemaRecorder.cs
+++ src/Renode/Integrations/AsciinemaRecorder.cs
@@ -13,7 +13,9 @@ using System.Text;
 using Antmicro.Migrant;
 using Antmicro.Renode.Core;
 using Antmicro.Renode.Exceptions;
+using Antmicro.Renode.Peripherals;
 using Antmicro.Renode.Peripherals.UART;
+using Antmicro.Renode.Peripherals.USB;
 using Antmicro.Renode.Utilities;
 using TermSharp.Vt100;
 
@@ -24,7 +26,29 @@ namespace Antmicro.Renode.Integrations
         public static void RecordToAsciinema(this IUART uart, string filePath, bool useVirtualTimeStamps = false, int width = 80, int height = 24)
         {
             var emulation = EmulationManager.Instance.CurrentEmulation;
-            if(!emulation.TryGetMachineForPeripheral(uart, out var machine))
+            IMachine machine = null;
+            // This is a temporary solution and should be addressed later
+            var cdcAcmUart = uart as CDCToUARTConverter;
+            if(cdcAcmUart != null)
+            {
+                // In USB, the CDC ACM UART isn't enabled before enumeration.
+                // We use Children here, assuming that after enumeration the
+                // UART will be available.
+                var devices = cdcAcmUart.Children;
+                foreach(var device in devices)
+                {
+                    if(emulation.TryGetMachineForPeripheral(device.Peripheral, out machine))
+                    {
+                        break;
+                    }
+                }
+                
+                if(machine == null)
+                {
+                    throw new RecoverableException("Could not find machine for the given CDC ACM UART");
+                }
+            }
+            else if(!emulation.TryGetMachineForPeripheral(uart, out machine))
             {
                 throw new RecoverableException("Could not find machine for the given UART");
             }
