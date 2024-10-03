c8eef4ab64ea2d81babaa0a2c590a04256c00d63
Lilly Jinstrand <ljinstrand@antmicro.com>
2024-10-03 12:45:55 +0200

    [#65543] Refactor so build.sh only calls cmake in one place

--- build.sh
+++ build.sh
@@ -299,23 +299,22 @@ CORES=(arm.le arm.be arm64.le arm-m.le arm-m.be ppc.le ppc.be ppc64.le ppc64.be 
 # build tlib
 for core_config in "${CORES[@]}"
 do
-    core="$(echo $core_config | cut -d '.' -f 1)"
-    endian="$(echo $core_config | cut -d '.' -f 2)"
+    CORE="$(echo $core_config | cut -d '.' -f 1)"
+    ENDIAN="$(echo $core_config | cut -d '.' -f 2)"
     BITS=32
     # Check if core is 64-bit
-    if [[ $core =~ "64" ]]; then
+    if [[ $CORE =~ "64" ]]; then
       BITS=64
     fi
     # Core specific flags to cmake
-    CMAKE_CONF_FLAGS="-DTARGET_ARCH=$core -DTARGET_WORD_SIZE=$BITS -DCMAKE_BUILD_TYPE=$CONFIGURATION"
-    CORE_DIR=$CORES_BUILD_PATH/$core/$endian
+    CMAKE_CONF_FLAGS="-DTARGET_ARCH=$CORE -DTARGET_WORD_SIZE=$BITS -DCMAKE_BUILD_TYPE=$CONFIGURATION"
+    CORE_DIR=$CORES_BUILD_PATH/$CORE/$ENDIAN
     mkdir -p $CORE_DIR
     pushd "$CORE_DIR" > /dev/null
-    if [[ $endian == "be" ]]; then
-      cmake "$CMAKE_COMMON" $CMAKE_CONF_FLAGS -DTARGET_BIG_ENDIAN=1 -DHOST_ARCH=$HOST_ARCH $CORES_PATH
-    else
-      cmake "$CMAKE_COMMON" $CMAKE_CONF_FLAGS -DHOST_ARCH=$HOST_ARCH $CORES_PATH
+    if [[ $ENDIAN == "be" ]]; then
+        CMAKE_CONF_FLAGS+=" -DTARGET_BIG_ENDIAN=1"
     fi
+    cmake "$CMAKE_COMMON" $CMAKE_CONF_FLAGS -DHOST_ARCH=$HOST_ARCH $CORES_PATH
     cmake --build . -j$(nproc)
     CORE_BIN_DIR=$CORES_BIN_PATH/lib
     mkdir -p $CORE_BIN_DIR
