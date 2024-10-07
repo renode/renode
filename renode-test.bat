436226983cbf2b3907260cb5d2422d338cc76083
Andrzej Drabarek <adrabarek@antmicro.com>
2024-10-07 11:42:40 +0200

    fixup! merge test.bat and renode-test.bat

--- renode-test.bat
+++ renode-test.bat
@@ -12,13 +12,13 @@ shift
 goto args
 :args_end
 
-set "BINDIR=%SCRIPTDIR%..\output\bin\Release"
+set "BINDIR=%SCRIPTDIR%\output\bin\Release"
 if "%DEBUG%" == "1" (
-    set "BINDIR=%SCRIPTDIR%..\output\bin\Debug"
+    set "BINDIR=%SCRIPTDIR%\output\bin\Debug"
 )
 
 if "%CONTEXT%" == "source" (
-    py -3 "%SCRIPTDIR%\run_tests.py" --css-file "%SCRIPTDIR%\..\lib\resources\styles\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%BINDIR%" -r %cd% %*
+    py -3 "%SCRIPTDIR%\tests\run_tests.py" --css-file "%SCRIPTDIR%\lib\resources\styles\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%BINDIR%" -r %cd% %*
 ) else (
     py -3 "%SCRIPTDIR%\..\tests\run_tests.py" --css-file "%SCRIPTDIR%\..\tests\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%SCRIPTDIR%" -r %cd% %*
 )
