bf4ae10050ccf1c2a4b714d4f0c4f321ff0c6c2a
Andrzej Drabarek <adrabarek@antmicro.com>
2024-10-21 10:31:51 +0200

    Rename verilator-integration-library to cosimulation-library

--- src/Plugins/CoSimulationPlugin/CoSimulationLibrary/verilator-integration-library.cmake
+++ src/Plugins/CoSimulationPlugin/CoSimulationLibrary/verilator-integration-library.cmake
@@ -1,3 +1,5 @@
 # To build a cosimulated peripheral, see https://github.com/antmicro/renode-verilator-integration
 
-set(VIL_VERSION 2.0)
+# This file is deprecated.
+message(DEPRECATION "VerilatorIntegrationLibrary has been renamed to CoSimulationLibrary. Please use cosimulation-library.cmake instead of verilator-integration-library.cmake")
+set(VIL_VERSION 3.0)
