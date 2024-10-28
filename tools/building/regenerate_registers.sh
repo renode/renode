51a2a2d4662a8cc2dddf0c7f3bb91cef36e031e9
Franciszek Pindel <fpindel@antmicro.com>
2024-10-28 13:52:15 +0100

    [#66348] regenerate_registers: Add X86KVM CPU

--- tools/building/regenerate_registers.sh
+++ tools/building/regenerate_registers.sh
@@ -7,7 +7,7 @@ THIS_DIR="$(cd $(dirname $0); echo $PWD)"
 CORES_PATH=$THIS_DIR/../../src/Infrastructure/src/Emulator/Cores
 BASE_PATH=$THIS_DIR/../../src/Infrastructure/src/Emulator/Cores/Common/
 
-FILES=(Sparc/Sparc Arm/Arm Arm64/ARMv8A Arm64/ARMv8R Arm-M/CortexM PowerPC/PowerPc PowerPC/PowerPc64 RiscV/RiscV32 RiscV/RiscV64 X86/X86 Xtensa/Xtensa X86/X86_64)
+FILES=(Sparc/Sparc Arm/Arm Arm64/ARMv8A Arm64/ARMv8R Arm-M/CortexM PowerPC/PowerPc PowerPC/PowerPc64 RiscV/RiscV32 RiscV/RiscV64 X86/X86 Xtensa/Xtensa X86/X86_64 X86KVM/X86KVM)
 
 for file in ${FILES[@]}; do
     # dotnet-t4 tool used to process T4 templates must be installed separately:
