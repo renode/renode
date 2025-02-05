b653638531769f267ea566cdf6146303c946543a
Mateusz Karlic <mkarlic@antmicro.com>
2025-02-05 15:11:55 +0100

    [#71474] execution_tracer: Add launch alias

---
+++ tools/execution_tracer/execution_tracer_reader.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2010-2025 Antmicro
+#
+# This file is licensed under the MIT License.
+# Full license text is available in 'licenses/MIT.txt'.
+#
+
+from execution_tracer.execution_tracer_reader import main
+
+if __name__ == "__main__":
+    main()
