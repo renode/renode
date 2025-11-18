98b3f89ef8f615d45b56d419005a390cd5893762
Zee Yudenko <zyudenko@internships.antmicro.com>
2025-11-18 15:34:46 +0100

    [#86917] Treat null source as empty source

--- src/Renode/PlatformDescription/UsingsGraph.cs
+++ src/Renode/PlatformDescription/UsingsGraph.cs
@@ -116,6 +116,12 @@ namespace Antmicro.Renode.PlatformDescription
 
             private GraphNode CreateGraphNode(CreationDriver creationDriver, string filePath, string source, string prefix, GraphNode parent)
             {
+                if(source == null)
+                {
+                    // Treat null `source` as an empty source
+                    source = "";
+                }
+
                 if(source == "" && filePath != "")
                 {
                     source = File.ReadAllText(filePath);
