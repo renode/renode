46414dd4a25af8195087899b96174ba4dacf7846
Patryk Koscik <pkoscik@antmicro.com>
2026-02-11 22:46:35 +0000

    [#75284] ZephyrMode: Add support for `RAM console`

--- src/Renode/Plugins/ZephyrMode.cs
+++ src/Renode/Plugins/ZephyrMode.cs
@@ -8,8 +8,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+using Antmicro.Renode.Core.Structure;
+using Antmicro.Renode.Exceptions;
 using Antmicro.Renode.Logging;
 using Antmicro.Renode.Peripherals.CPU;
+using Antmicro.Renode.Peripherals.UART;
 
 namespace Antmicro.Renode.Peripherals.Plugins
 {
@@ -41,6 +44,48 @@ namespace Antmicro.Renode.Peripherals.Plugins
         {
             OsSymbolHook.Disable(cpu, "z_impl_k_busy_wait");
         }
+
+        public static void CreateZephyrRamConsole(this ICpuSupportingGdb cpu)
+        {
+            var machine = cpu.GetMachine();
+            if(machine.TryGetByName<VirtualConsole>($"sysbus.{ramConsoleVirtualConsoleName}", out var _))
+            {
+                throw new RecoverableException($"Failed to register: '{ramConsoleVirtualConsoleName}' - device with provided name already exists!");
+                return;
+            }
+
+            var console = new VirtualConsole(machine);
+            machine.RegisterAsAChildOf(machine.SystemBus, console, NullRegistrationPoint.Instance);
+            machine.SetLocalName(console, ramConsoleVirtualConsoleName);
+            Logger.Log(LogLevel.Info, $"Created Zephyr RAM console peripheral: '{ramConsoleVirtualConsoleName}'");
+
+            OsSymbolHook.Enable(cpu, ramConsoleSymbolName, (ICpuSupportingGdb c, ulong __) =>
+            {
+                if(!OsSymbolHook.TryGetFirstParameter(cpu, out var firstParameter))
+                {
+                    Logger.Log(LogLevel.Warning, "Unable to get RAM console out first parameter");
+                    return;
+                }
+                var charValue = (byte)firstParameter;
+                console.WriteChar(charValue);
+            });
+        }
+
+        public static void RemoveZephyrRamConsole(this ICPU cpu)
+        {
+            var machine = cpu.GetMachine();
+            if(!machine.TryGetByName<VirtualConsole>($"sysbus.{ramConsoleVirtualConsoleName}", out var console))
+            {
+                throw new RecoverableException($"Failed to unregister: '{ramConsoleVirtualConsoleName}' - device with provided name not found!");
+                return;
+            }
+
+            machine.UnregisterFromParent(console);
+            OsSymbolHook.Disable(cpu, ramConsoleSymbolName);
+        }
+
+        private static readonly string ramConsoleSymbolName = "ram_console_out";
+        private static readonly string ramConsoleVirtualConsoleName = "zephyr_ram_console";
 
         private static readonly List<string> unsupportedSymbols = new List<string> {
             "CONFIG_TICKLESS_KERNEL",
