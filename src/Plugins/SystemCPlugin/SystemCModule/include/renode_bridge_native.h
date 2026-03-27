db64c24ba83207e8b26777beeab19a7cb6c8e71f
Dawid Olszewski <dolszewski@antmicro.com>
2026-03-27 13:04:23 +0100

    [#93517] SystemCModule: Explicitly export API symbols

--- src/Plugins/SystemCPlugin/SystemCModule/include/renode_bridge_native.h
+++ src/Plugins/SystemCPlugin/SystemCModule/include/renode_bridge_native.h
@@ -10,6 +10,7 @@
 #include <optional>
 #include <systemc>
 #include <tlm>
+#include "renode_bridge_native_export.h"
 
 struct IRenodeBridge {
   virtual void reset() = 0;
@@ -28,19 +29,19 @@ using renode_bridge_factory_t = IRenodeBridge* (*)();
 void register_renode_bridge_factory(renode_bridge_factory_t);
 
 extern "C" {
-  void systemc_init();
-  void systemc_reset();
-  void systemc_start_sim(int ns);
+  RENODE_BRIDGE_API void systemc_init();
+  RENODE_BRIDGE_API void systemc_reset();
+  RENODE_BRIDGE_API void systemc_start_sim(int ns);
 
-  std::uint64_t tlm_read(std::size_t size, std::uint64_t offset);
-  void tlm_write(std::size_t size, std::int64_t value, std::uint64_t offset);
+  RENODE_BRIDGE_API std::uint64_t tlm_read(std::size_t size, std::uint64_t offset);
+  RENODE_BRIDGE_API void tlm_write(std::size_t size, std::int64_t value, std::uint64_t offset);
 
-  void gpio_write(int number, bool value);
-  void renode_gpio_update(int number, int value);
+  RENODE_BRIDGE_API void gpio_write(int number, bool value);
+  RENODE_BRIDGE_API void renode_gpio_update(int number, int value);
 
-  void renode_invalidate_translation_blocks(std::uint64_t start_address, std::uint64_t end_address);
-  void renode_read_bytes_from_bus(std::uint64_t address, void *out_buf, std::int32_t count);
-  void renode_write_bytes_to_bus(std::uint64_t address, void *in_buf, std::int32_t count);
+  RENODE_BRIDGE_API void renode_invalidate_translation_blocks(std::uint64_t start_address, std::uint64_t end_address);
+  RENODE_BRIDGE_API void renode_read_bytes_from_bus(std::uint64_t address, void *out_buf, std::int32_t count);
+  RENODE_BRIDGE_API void renode_write_bytes_to_bus(std::uint64_t address, void *in_buf, std::int32_t count);
 }
 
 std::optional<DmiRegion> renode_get_direct_mem_ptr(uint64_t address);
