b9d248f7ac6f39e45e539141c3c4d5052bb47191
Zee Yudenko <zyudenko@internships.antmicro.com>
2026-04-09 15:53:34 +0200

    [DEV] renode-test.bat: Remove unused logic and misleading comment

--- renode-test.bat
+++ renode-test.bat
@@ -1,6 +1,4 @@
 @echo off
-REM We change the below line to "set CONTEXT=package" during package generation
-set "CONTEXT=source"
 set "SCRIPTDIR=%~dp0"
 
 :args
@@ -17,8 +15,4 @@ if "%DEBUG%" == "1" (
     set "BINDIR=%SCRIPTDIR%\output\bin\Debug"
 )
 
-if "%CONTEXT%" == "source" (
-    py -3 "%SCRIPTDIR%\tests\run_tests.py" --css-file "%SCRIPTDIR%\lib\resources\styles\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%BINDIR%" -r %cd% %*
-) else (
-    py -3 "%SCRIPTDIR%\..\tests\run_tests.py" --css-file "%SCRIPTDIR%\..\tests\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%SCRIPTDIR%\" --robot-framework-remote-server-name "renode.exe" -r "%cd%" %*
-)
+py -3 "%SCRIPTDIR%\tests\run_tests.py" --css-file "%SCRIPTDIR%\lib\resources\styles\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%BINDIR%" -r %cd% %*
