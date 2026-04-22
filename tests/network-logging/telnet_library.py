744aa05cb6c4bd463c88e70006add7a4ea264d6e
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2026-04-22 16:47:36 +0200

    [#95046] telnet_library: Add telnet_write

--- tests/network-logging/telnet_library.py
+++ tests/network-logging/telnet_library.py
@@ -1,23 +1,26 @@
-import socket
 import asyncio
-from telnetlib3 import open_connection, TelnetReader
+from telnetlib3 import open_connection, TelnetReader, TelnetWriter
 
 ENCODING = "utf-8"
 
 reader: TelnetReader
+writer: TelnetWriter
 
 def telnet_connect(port: int) -> None:
-    global reader
-    
+    global reader, writer
+
     loop = asyncio.get_event_loop()
-    
+
     # Coroutines with event loop required for robot tests
     coro = open_connection('localhost', port)
-    reader, _ = loop.run_until_complete(coro)
+    reader, writer = loop.run_until_complete(coro)  # type: ignore
+
+def telnet_write(text: str) -> None:
+    loop = asyncio.get_event_loop()
+    writer.write(text)
+    loop.run_until_complete(writer.drain())
     
 def telnet_read_until(until_string: str, timeout: int = 15) -> str:
-    global reader
-    
     loop = asyncio.get_event_loop()
 
     # Wrap the readuntil coroutine with wait_for to add timeout
