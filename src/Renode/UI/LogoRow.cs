ac7479fc8c0c151af2cc674c0b47b98e9a828e30
Pawel Reich <preich@internships.antmicro.com>
2026-05-08 18:11:44 +0200

    [#96404] UI: Remove old MonoDevelop warning

--- src/Renode/UI/LogoRow.cs
+++ src/Renode/UI/LogoRow.cs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2010-2018 Antmicro
+// Copyright (c) 2010-2026 Antmicro
 //
 // This file is licensed under the MIT License.
 // Full license text is available in 'licenses/MIT.txt'.
@@ -24,11 +24,7 @@ namespace Antmicro.Renode.UI
         public override double PrepareForDrawing(ILayoutParameters parameters)
         {
             var baseResult = base.PrepareForDrawing(parameters);
-#pragma warning disable RECS0018 // Comparison of floating point numbers with equality operator
-            // MonoDevelop complains about comparing double to a const int,
-            // but we detect uninitialized case here, so it's ok.
             if(LineHeight == 0) // UI has not been initalized yet
-#pragma warning restore RECS0018 // Comparison of floating point numbers with equality operator
             {
                 return baseResult;
             }
