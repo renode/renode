83db67c465ff5492b39c041c31f794b6f562bd0b
Bartlomiej Stefanski <bstefanski@internships.antmicro.com>
2026-05-12 11:49:38 +0200

    [#95731] Do not force global Python environment in batch files

--- renode-test.bat
+++ renode-test.bat
@@ -15,4 +15,14 @@ if "%DEBUG%" == "1" (
     set "BINDIR=%SCRIPTDIR%\output\bin\Debug"
 )
 
-py -3 "%SCRIPTDIR%\tests\run_tests.py" --css-file "%SCRIPTDIR%\lib\resources\styles\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%BINDIR%" -r "%cd%" %*
+where py >nul 2>&1
+if "%ERRORLEVEL%" == "0" (
+    REM Before Python3.14 'py' standed for "Python launcher for Windows", which expects 'PY_PYTHON'.
+    REM For Python3.14 and later 'py' stands for "Python install manager", which expects 'PYTHON_MANAGER_DEFAULT'.
+    set "PYTHON_MANAGER_DEFAULT=3"
+    set "PY_PYTHON=3"
+    
+    py "%SCRIPTDIR%\tests\run_tests.py" --css-file "%SCRIPTDIR%\lib\resources\styles\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%BINDIR%" -r "%cd%" %*
+) else (
+    python3 "%SCRIPTDIR%\tests\run_tests.py" --css-file "%SCRIPTDIR%\lib\resources\styles\robot.css" --exclude "skip_windows" --robot-framework-remote-server-full-directory "%BINDIR%" -r "%cd%" %*
+)
--- tools/packaging/windows/test.bat
+++ tools/packaging/windows/test.bat
@@ -1,4 +1,15 @@
 @echo off
 set "SCRIPTDIR=%~dp0"
 set "BINDIR=%SCRIPTDIR%REPLACE_BIN_DIR"
-py -3 "%SCRIPTDIR%run_tests.py" --css-file "%SCRIPTDIR%robot.css" --exclude "skip_windows" --exclude "skip_portable" --robot-framework-remote-server-full-directory "%BINDIR%" --robot-framework-remote-server-name=REPLACE_BIN_NAME -r "%cd%" %*
+
+where py >nul 2>&1
+if "%ERRORLEVEL%" == "0" (
+    REM Before Python3.14 'py' standed for "Python launcher for Windows", which expects 'PY_PYTHON'.
+    REM For Python3.14 and later 'py' stands for "Python install manager", which expects 'PYTHON_MANAGER_DEFAULT'.
+    set "PYTHON_MANAGER_DEFAULT=3"
+    set "PY_PYTHON=3"
+
+    py "%SCRIPTDIR%run_tests.py" --css-file "%SCRIPTDIR%robot.css" --exclude "skip_windows" --exclude "skip_portable" --robot-framework-remote-server-full-directory "%BINDIR%" --robot-framework-remote-server-name=REPLACE_BIN_NAME -r "%cd%" %*
+) else (
+    python3 "%SCRIPTDIR%run_tests.py" --css-file "%SCRIPTDIR%robot.css" --exclude "skip_windows" --exclude "skip_portable" --robot-framework-remote-server-full-directory "%BINDIR%" --robot-framework-remote-server-name=REPLACE_BIN_NAME -r "%cd%" %*
+)
