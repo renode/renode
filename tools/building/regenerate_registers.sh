31b49ca750b2995469eda383ea6ad29b3c045881
Adam Jeliński <ajelinski@antmicro.com>
2026-06-23 13:12:18 +0200

    [DEV] regenerate_registers: Improve behavior without dotnet-t4 installed

--- tools/building/regenerate_registers.sh
+++ tools/building/regenerate_registers.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
+
+set -eu
+
+# dotnet-t4 tool used to process T4 templates must be installed separately
+# https://github.com/mono/t4/pkgs/nuget/dotnet-t4
+if ! (t4 -h &>/dev/null); then
+    exec >&2
+    echo "dotnet-t4 tool necessary to regenerate source files is missing."
+    echo
+    echo "It can be installed with:"
+    echo "  dotnet tool install -g dotnet-t4"
+    exit 1
+fi
+
 set -x
-set -e
 
 THIS_DIR="$(cd $(dirname $0); echo $PWD)"
 
@@ -10,9 +23,6 @@ BASE_PATH=$THIS_DIR/../../src/Infrastructure/src/Emulator/Cores/Common/
 FILES=(Sparc/Sparc Arm/Arm Arm64/ARMv8A Arm64/ARMv8R Arm-M/CortexM PowerPC/PowerPc PowerPC/PowerPc64 RiscV/RiscV32 RiscV/RiscV64 X86/X86 Xtensa/Xtensa X86/X86_64 KVM/X86/X86KVM KVM/X86/X86_64KVM)
 
 for file in ${FILES[@]}; do
-    # dotnet-t4 tool used to process T4 templates must be installed separately:
-    # dotnet tool install -g dotnet-t4
-    # https://github.com/mono/t4/pkgs/nuget/dotnet-t4
     t4 -p:BASE_PATH=$BASE_PATH -o $CORES_PATH/${file}Registers.{cs,tt}
     # Remove trailing invisible newline
     truncate -s -1 $CORES_PATH/${file}Registers.cs
