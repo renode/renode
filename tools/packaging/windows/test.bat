b13efbae1ba0817d73526543d80298dd87f02c01
Bartlomiej Stefanski <bstefanski@internships.antmicro.com>
2026-07-09 14:24:49 +0200

    [#95731] Run python directly in batch files

--- tools/packaging/windows/test.bat
+++ tools/packaging/windows/test.bat
@@ -5,14 +5,5 @@ set "BINDIR=%SCRIPTDIR%REPLACE_BIN_DIR"
 set "CWD=%cd%"
 if "%CWD:~-1%"=="\" set "CWD=%CWD%."
 
-where py >nul 2>&1
-if "%ERRORLEVEL%" == "0" (
-    REM Before Python3.14 'py' standed for "Python launcher for Windows", which expects 'PY_PYTHON'.
-    REM For Python3.14 and later 'py' stands for "Python install manager", which expects 'PYTHON_MANAGER_DEFAULT'.
-    set "PYTHON_MANAGER_DEFAULT=3"
-    set "PY_PYTHON=3"
-
-    py "%SCRIPTDIR%run_tests.py" --css-file "%SCRIPTDIR%robot.css" --exclude "skip_windows" --exclude "skip_portable" --robot-framework-remote-server-full-directory "%BINDIR%" --robot-framework-remote-server-name=REPLACE_BIN_NAME -r "%CWD%" %*
-) else (
-    python3 "%SCRIPTDIR%run_tests.py" --css-file "%SCRIPTDIR%robot.css" --exclude "skip_windows" --exclude "skip_portable" --robot-framework-remote-server-full-directory "%BINDIR%" --robot-framework-remote-server-name=REPLACE_BIN_NAME -r "%CWD%" %*
-)
+REM The user is responsible for preaparing an appropriate runtime environment.
+python "%SCRIPTDIR%run_tests.py" --css-file "%SCRIPTDIR%robot.css" --exclude "skip_windows" --exclude "skip_portable" --robot-framework-remote-server-full-directory "%BINDIR%" --robot-framework-remote-server-name=REPLACE_BIN_NAME -r "%CWD%" %*
