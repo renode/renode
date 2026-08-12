fede349dac1a5b84ae2575dba2c6f9335669d9ba
Jan Olencki <jolencki@antmicro.com>
2026-08-12 10:14:14 +0200

    [#102295] external_control: Simplify RunFor command
    Co-Authored-By: Jan Malek <jmalek@antmicro.com>

--- src/Renode/Network/ExternalControl/RunFor.cs
+++ src/Renode/Network/ExternalControl/RunFor.cs
@@ -6,7 +6,7 @@
 //
 using System;
 using System.Collections.Generic;
-using System.Threading;
+using System.Runtime.InteropServices;
 
 using Antmicro.Renode.Core;
 using Antmicro.Renode.Logging;
@@ -14,94 +14,29 @@ using Antmicro.Renode.Time;
 
 namespace Antmicro.Renode.Network.ExternalControl
 {
-    public class RunFor : BaseCommand, IDisposable
+    public class RunFor : BaseCommand
     {
         public RunFor(ExternalControlServer parent)
             : base(parent)
         {
         }
 
-        public void Dispose()
-        {
-            if(cancellationToken != null)
-            {
-                parent.Log(LogLevel.Warning, "RunFor disposed while running");
-            }
-            cancellationToken?.Cancel();
-            cancellationToken = null;
-            disposed = true;
-        }
-
         public override MessagePayload Invoke(List<byte> data)
         {
-            if(disposed)
-            {
-                return MessagePayload.Error(Identifier, "Command is unavailable");
-            }
-
             if(data.Count != 8)
             {
                 return MessagePayload.Error(Identifier, "Expected 8 bytes of payload");
             }
 
-            if(cancellationToken != null)
-            {
-                return MessagePayload.Error(Identifier, "One RunFor command can be running at any given time");
-            }
-
-            cancellationToken = new CancellationTokenSource();
-            exception = null;
-            success = false;
-
-            var nanoseconds = BitConverter.ToUInt64(data.ToArray(), 0);
+            var nanoseconds = BitConverter.ToUInt64(CollectionsMarshal.AsSpan(data));
             var interval = TimeInterval.FromNanoseconds(nanoseconds);
 
-            var thread = new Thread(() =>
-            {
-                try
-                {
-                    parent.Log(LogLevel.Info, "Executing RunFor({0}) command", interval);
-                    EmulationManager.Instance.CurrentEmulation.RunFor(interval);
+            parent.Log(LogLevel.Info, "Executing RunFor({0}) command", interval);
+            EmulationManager.Instance.CurrentEmulation.RunFor(interval);
 
-                    if(cancellationToken.IsCancellationRequested)
-                    {
-                        return;
-                    }
-
-                    success = true;
-                }
-                catch(Exception e)
-                {
-                    exception = e;
-                }
-                cancellationToken?.Cancel();
-            })
-            {
-                IsBackground = true,
-                Name = GetType().Name
-            };
-
-            thread.Start();
-            cancellationToken.Token.WaitHandle.WaitOne();
-            cancellationToken = null;
-
-            if(exception != null)
-            {
-                throw exception;
-            }
-
-            if(success)
-            {
-                return MessagePayload.Success(Identifier);
-            }
-            return MessagePayload.Error(Identifier, "RunFor was interrupted");
+            return MessagePayload.Success(Identifier);
         }
 
         public override Command Identifier => Command.RunFor;
-
-        private bool success;
-        private Exception exception;
-        private CancellationTokenSource cancellationToken;
-        private bool disposed;
     }
 }
